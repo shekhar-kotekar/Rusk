f3fa320c8aaebca756c74b1dedb876f744bbe411
shekhar-kotekar <shekhar.kotekar@gmail.com>
2024-06-22 00:46:24 +0200

    sample unit test added for axum web server

--- rusk_web/src/main.rs
+++ rusk_web/src/main.rs
@@ -12,3 +12,18 @@ async fn main() {
 async fn root() -> &'static str {
     "Hello, World!"
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{routing::get, Router};
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn test_server() {
+        let app = Router::new().route("/", get(super::root));
+        let test_server = TestServer::new(app).unwrap();
+        let response = test_server.get("/").await;
+        response.assert_status_ok();
+        response.assert_text("Hello, World!");
+    }
+}
