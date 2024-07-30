ad6a6255e80e8933e69169d73ea9f7b670ba9c25
shekhar-kotekar <shekhar.kotekar@gmail.com>
2024-07-30 22:28:41 +0200

    cancellation token added to content repository

--- content_repository/src/main.rs
+++ content_repository/src/main.rs
@@ -1,10 +1,12 @@
 use bytes::Bytes;
 use models::Command;
 use tokio::{
-    io::{AsyncRead, AsyncReadExt, AsyncSeekExt},
+    io::{AsyncRead, AsyncReadExt, AsyncSeekExt, AsyncWriteExt},
     net::TcpListener,
+    select,
     sync::{mpsc, oneshot},
 };
+use tokio_util::sync::CancellationToken;
 
 mod content_repository_manager;
 mod models;
@@ -21,34 +23,44 @@ async fn main() {
     tracing::info!("Rusk content repository listening on {}", server_address);
 
     let mut file_handle = content_repository_manager::init(config).await;
+
     let (conten_repo_manager_tx, mut content_repo_manager_rx) = mpsc::channel::<Command>(1000);
+
+    let cancellation_token = CancellationToken::new();
+    let cancellation_token_for_content_repo_manager = cancellation_token.clone();
 
     tokio::spawn(async move {
         while let Some(data) = content_repo_manager_rx.recv().await {
-            spawn_content_repo_manager(data, &mut file_handle).await;
+            if cancellation_token_for_content_repo_manager.is_cancelled() {
+                tracing::info!("Cancellation token received. Stopping content repo manager task.");
+                break;
+            }
+            process_data(data, &mut file_handle).await;
         }
     });
 
-    // loop {
-    //     tracing::info!("Waiting for new client connection...");
-    //     let (mut socket, _) = listener.accept().await.unwrap();
-    //     tracing::info!("New client connected: {:?}", socket.peer_addr().unwrap());
+    accept_client_connections(listener, conten_repo_manager_tx, cancellation_token.clone()).await;
 
-    //     let conten_repo_manager_tx_clone = conten_repo_manager_tx.clone();
-    //     tokio::spawn(async move {
-    //         let (reader, _) = socket.split();
-    //         handle_client_request(conten_repo_manager_tx_clone, reader).await;
-    //     });
-    // }
-    accept_client_connections(listener, conten_repo_manager_tx.clone()).await;
+    match tokio::signal::ctrl_c().await {
+        Ok(_) => {
+            tracing::info!("Ctrl-C signal received. Shutting down content repository server.");
+            cancellation_token.cancel();
+        }
+        Err(e) => {
+            tracing::error!("Failed to listen for Ctrl-C signal: {:?}", e);
+        }
+    }
 }
 
-async fn spawn_content_repo_manager(data: Command, file_handle: &mut tokio::fs::File) {
+async fn process_data(data: Command, file_handle: &mut tokio::fs::File) {
     match data {
-        Command::Data { content, tx } => {
-            let current_offset = file_handle.stream_position().await.unwrap();
+        Command::Data {
+            content,
+            tx: oneshot_tx,
+        } => {
+            let record_start_offset = file_handle.stream_position().await.unwrap();
             content_repository_manager::append_data(file_handle, &content).await;
-            let _ = tx.send(current_offset);
+            let _ = oneshot_tx.send(record_start_offset);
         }
     }
 }
@@ -56,21 +68,44 @@ async fn spawn_content_repo_manager(data: Command, file_handle: &mut tokio::fs::
 async fn accept_client_connections(
     listener: TcpListener,
     conten_repo_manager_tx: mpsc::Sender<Command>,
+    cancellation_token: CancellationToken,
 ) {
     loop {
         tracing::info!("Waiting for new client connection...");
-        let (mut socket, _) = listener.accept().await.unwrap();
-        tracing::info!("New client connected: {:?}", socket.peer_addr().unwrap());
+        select! {
+            _ = cancellation_token.cancelled() => {
+                tracing::info!("Cancellation token received. Stopping client connection listener.");
+                break;
+            }
+            Ok((mut socket, _)) = listener.accept() => {
+                tracing::info!("New client connected: {:?}", socket.peer_addr().unwrap());
 
-        let conten_repo_manager_tx_clone = conten_repo_manager_tx.clone();
-        tokio::spawn(async move {
-            let (reader, _) = socket.split();
-            handle_client_request(conten_repo_manager_tx_clone, reader).await;
-        });
+                let conten_repo_manager_tx_clone = conten_repo_manager_tx.clone();
+                tokio::spawn(async move {
+                    let (reader, mut writer) = socket.split();
+                    // TODO: Do we need to send cancellation token here as well?
+                    match handle_client_request(conten_repo_manager_tx_clone, reader).await {
+                        Some(response) => {
+                            tracing::info!("Response from content repo: {:?}", response);
+                            writer.write_u64(response).await.unwrap();
+                        }
+                        None => {
+                            tracing::info!("Client connection closed.");
+                            writer.write_u64(0).await.unwrap();
+                        }
+                    }
+                    writer.flush().await.unwrap();
+                    writer.shutdown().await.unwrap();
+                });
+            }
+        }
     }
 }
 
-async fn handle_client_request<Reader>(tx_clone: mpsc::Sender<Command>, mut reader: Reader)
+async fn handle_client_request<Reader>(
+    tx_clone: mpsc::Sender<Command>,
+    mut reader: Reader,
+) -> Option<u64>
 where
     Reader: AsyncRead + Unpin,
 {
@@ -79,7 +114,7 @@ where
         if let Ok(bytes_read) = reader.read_buf(&mut buffer).await {
             if bytes_read == 0 {
                 tracing::info!("0 bytes read, connection closed.");
-                break;
+                return None;
             }
             let (one_shot_tx, one_shot_rx) = oneshot::channel::<u64>();
             let command = Command::Data {
@@ -89,6 +124,7 @@ where
             tx_clone.send(command).await.unwrap();
             let response = one_shot_rx.await.unwrap();
             tracing::info!("Response from content repo: {:?}", response);
+            return Some(response);
         }
     }
 }
@@ -97,7 +133,7 @@ where
 mod tests {
     use commons::ContentRepositoryConfig;
     use tempfile::tempdir;
-    use tokio::{io::AsyncWriteExt, net::TcpStream, signal};
+    use tokio::{io::AsyncWriteExt, net::TcpStream};
 
     use super::*;
     use std::{fs::File as StdFile, io::Read};
@@ -106,47 +142,17 @@ mod tests {
     async fn test_accept_client_connections() {
         let listener = TcpListener::bind("127.0.0.1:5056").await.unwrap();
         let addr = listener.local_addr().unwrap();
-
+        let cancellation_token = CancellationToken::new();
+        let cancellation_token_clone = cancellation_token.clone();
         let (tx, _) = mpsc::channel::<Command>(10);
-        let client_connection_handle = tokio::spawn(async move {
-            accept_client_connections(listener, tx).await;
+
+        tokio::spawn(async move {
+            accept_client_connections(listener, tx, cancellation_token_clone).await;
         });
 
         let mut stream = TcpStream::connect(addr).await.unwrap();
         stream.write_all(b"data_to_write").await.unwrap();
 
-        match signal::ctrl_c().await {
-            Ok(_) => {
-                client_connection_handle.abort();
-            }
-            Err(e) => {
-                panic!("Error: {:?}", e);
-            }
-        }
-    }
-
-    #[tokio::test]
-    async fn test_spawn_content_repo_manager() {
-        let temp_dir = tempdir().unwrap();
-        let test_config = ContentRepositoryConfig {
-            base_path: temp_dir.path().to_str().unwrap().to_string(),
-            file_name_prefix: String::from("test_wal_process_data"),
-            server_port: 8080,
-        };
-        let mut file_handle = content_repository_manager::init(test_config).await;
-        let (tx, rx) = oneshot::channel::<u64>();
-        let data = Command::Data {
-            content: Bytes::from_static(b"test_data"),
-            tx,
-        };
-        spawn_content_repo_manager(data, &mut file_handle).await;
-
-        let test_file_path = temp_dir.path().join("test_wal_process_data.txt");
-        let mut file = StdFile::open(test_file_path.to_str().unwrap()).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        assert_eq!(contents, "test_data\n");
-        let response_from_process_data = rx.await.unwrap();
-        assert_eq!(response_from_process_data, 0);
+        cancellation_token.cancel();
     }
 }
